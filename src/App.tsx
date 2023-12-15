5b05d3064e8d2773fffa8a1ea8e443b87dfa0253
amarjyothi98 <amarjyothi526@gmail.com>
2023-12-15 23:55:28 +0530

    setting up the navigator:

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,11 @@ import {Platform, SafeAreaView, ScrollView, StatusBar} from 'react-native';
 
 import React from 'react';
 
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+
+import Home from './screens/Home';
+
 import FlatCards from './components/FlatCards';
 import ElevatedCards from './components/ElevatedCards';
 import FancyCards from './components/FancyCards';
@@ -11,6 +16,13 @@ import Toast from './components/Toast';
 import ToastButtons from './components/ToastButtons';
 import ToProfileBusinessButton from './components/ToProfileBusinessButton';
 import BusinessAroundProfile from './components/BusinessAroundProfile';
+
+export type RootStackParamList = {
+  Home: undefined;
+  Details: {productId: String};
+};
+
+const stack = createNativeStackNavigator<RootStackParamList>();
 
 export default function App() {
   return (
